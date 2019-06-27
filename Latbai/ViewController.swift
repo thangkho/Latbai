f1e35e7197bd08c36968b7398cf5b420be5a4792
KIEN Nguyen Trung (VTI.D1) <kien.nguyentrung@vti.com.vn>
2019-06-27 13:21:04 +0700

    Initial Commit

---
+++ Latbai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Latbai
+//
+//  Created by user1 on 6/27/19.
+//  Copyright © 2019 hoatv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
